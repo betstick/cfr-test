35691daf86073466d6cb17e3d5198c2f60698848
betstick <61669265+betstick@users.noreply.github.com>
2022-02-05 02:58:21 -0500

    i foreget

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "../lib/cfromreader/cfr.hpp"
+#include <signal.h>
 
 using namespace cfr;
 
@@ -25,19 +26,18 @@ int main()
 		hashMap.insert(bhd5->hashMap.begin(),bhd5->hashMap.end());
 	}
 	
-	printf("hashMap size: %i\n",hashMap.size());
-	printf("pathMap size: %i\n",pathMap.size());
-
+	//printf("hashMap size: %i\n",hashMap.size());
+	//printf("pathMap size: %i\n",pathMap.size());
 
 	// gywn: /chr/c5370.chrbnd.dcx
 
 	int32_t hash = pathMap.at("/chr/c5370.chrbnd.dcx");
 
-	printf("hash of /chr/c5370.chrbnd.dcx: %x\n",hash);
+	//printf("hash of /chr/c5370.chrbnd.dcx: %x\n",hash);
 
 	BHD5_1::FileHeader gywnHeader = hashMap.at((int32_t)0x8A67348D);
-	printf("offset of /chr/c5370.chrbnd.dcx: %lx\n",gywnHeader.offset);
-	printf("size of /chr/c5370.chrbnd.dcx: %lx\n",gywnHeader.fileSize);
+	//printf("offset of /chr/c5370.chrbnd.dcx: %lx\n",gywnHeader.offset);
+	//printf("size of /chr/c5370.chrbnd.dcx: %lx\n",gywnHeader.fileSize);
 
 	FILE* dvdbnd_bht  = fopen("../DATA/dvdbnd0.bdt","rb");
 	if(dvdbnd_bht == NULL)
@@ -48,10 +48,39 @@ int main()
 	fseek(dvdbnd_bht,gywnHeader.offset,SEEK_SET);
 	size_t arraySize;
 	char* bndRaw = openDCX(dvdbnd_bht,gywnHeader.fileSize,&arraySize);
+	MEM* bndMem = mopen(bndRaw,arraySize);
 
-	//fread(gwynChrBndDcxMem,gywnHeader.fileSize,1,dvdbnd_bht);
+	//int* fileCount;
+	BND3* bnd = openBnd3(bndMem);
 
-	char magic[4];
-	memcpy(magic,bndRaw,4);
-	printf("magic: %s\n",magic);
+	//printf("files: %i\n",bnd->header->fileCount);
+	//printf("file[0].rawFlags:%x\n",bnd->files[0].header->rawFlags);
+	//printf("file[0].compressedSize:%x\n",bnd->files[0].header->compressedSize);
+	//printf("file[0].dataStart:%x\n",bnd->files[0].data);
+
+	char magic[5];
+	memcpy(magic,bnd->files[0].data,5);
+	//printf("file[0] magic: %s\n",magic);
+
+	memcpy(magic,&bndMem->src[bnd->files[0].header->dataOffset],5);
+	//printf("file[0] magic: %s\n",magic);
+
+	mseek(bndMem,bnd->files[0].header->dataOffset,SEEK_SET);
+	FLVER2* flver = openFLVER2(bndMem);
+
+	printf("flver.header.meshcount:%i\n",flver->header->meshCount);
+	//printf("flver.dummy[8].color[0]:%i\n",flver->dummies[8].color[0]);
+	//printf("flver.bones[0].rot_x[0]:%f\n",flver->bones[0].rot_x);
+
+	printf("flver.vertexbuffers[0].vertices[0]:");
+	for(int i = 0; i < 40; i ++)
+	{
+		char c = 'x';
+		memcpy(&c,&flver->vertexBuffers[0].vertices[i],1);
+		printf("0x%u ",c);
+	}
+	printf("\n");
+
+	//exportFLVER2(*flver,"flver.txt");
+	//raise(SIGINT);
 };
